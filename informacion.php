f21d8e1eac86d42aa951aca35ae94b2df2cb5bd5
leydipulgarin86-png <leydipulgarin86@gmail.com>
2025-08-08 16:31:07 -0500

    informacion.php

---
+++ informacion.php
@@ -0,0 +1,10 @@
+<?php
+$Identificacion= $_POST [ "Identificacion"];
+$NomApellidos=$_POST [ "NomApellidos"];
+$cargo=$_POST ["cargo"];
+$salario=$_POST ["Salario"];
+echo "la identificacion del empleado: ". Identificacion;
+echo "los nombres y apellidos  del empleado so: ".NomApellidos;
+echo "el cargo del empleado es: ".Cargo";
+echo "El salario del empleado es:  "Salario;
+?>
